7ccf507c85631ba9eb7da565bcaa00a743b74102
dragonpaulz <paul.geesaman@gmail.com>
2020-03-10 10:49:20 -0400

    Creating server options class that parses command line arguments for use by the server class

---
+++ src/serveroptions.hpp
@@ -0,0 +1,11 @@
+#ifndef server_options_hpp_
+#define server_options_hpp_
+
+namespace setup_server {
+    class Server_Options {
+        public:
+        Server_Options(char**);
+    };
+}
+
+#endif
