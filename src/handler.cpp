87d6f4463b1bc7289b2523c3cc34df1ca71c2ab1
dragonpaulz <paul.geesaman@gmail.com>
2020-03-11 15:05:41 -0400

    Changing parameter type

--- src/handler.cpp
+++ src/handler.cpp
@@ -1,10 +1,9 @@
-#include <cstddef>
 #include <string>
 
 #include "handler.hpp"
 
 // When a stream of bytes come in, this is the code that interprets the data, and returns a string of bytes
-std::string HandleData::DataHandler::ReadBytes(std::byte[])
+std::string HandleData::DataHandler::ReadBytes(std::uint8_t[])
 {
     return "";
 }
--- src/handler.hpp
+++ src/handler.hpp
@@ -1,14 +1,13 @@
 #ifndef handler_hpp_
 #define handler_hpp_
 
-#include <cstddef>
 #include <string>
 
 namespace HandleData
 {
     class DataHandler{
         // Takes bytes of an unspecified length, and returns a string to interpret data
-        std::string ReadBytes(std::byte[]);
+        std::string ReadBytes(std::uint8_t[]);
         // Write out bytes to a specified stream
         void WriteBytes(std::string);
     };
