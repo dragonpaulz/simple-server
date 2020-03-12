4afc2e82cef907056645ce136df4a5f352416b60
dragonpaulz <paul.geesaman@gmail.com>
2020-03-12 16:32:41 -0400

    a bit of cleaning up

--- src/connection.cpp
+++ src/connection.cpp
@@ -8,31 +8,31 @@ using boost::asio::ip::tcp;
 class server_setup::Connection
   : public boost::enable_shared_from_this<server_setup::Connection>
 {
-public:
-  typedef boost::shared_ptr<server_setup::Connection> pointer;
+    public:
+    typedef boost::shared_ptr<server_setup::Connection> pointer;
 
-  static pointer create(boost::asio::io_context& io_context)
-  {
-    return pointer(new server_setup::Connection::Connection(io_context));
-  }
+    static pointer create(boost::asio::io_context& io_context)
+    {
+        return pointer(new server_setup::Connection::Connection(io_context));
+    }
 
-  tcp::socket& socket()
-  {
-    return socket_;
-  }
+    tcp::socket& socket()
+    {
+        return socket_;
+    }
 
-  void start()
-  {
-    // do something here
-  }
+    void start()
+    {
+        // do something here
+    }
 
-private:
+    private:
 
-  void handle_write(const boost::system::error_code& /*error*/,
-      size_t /*bytes_transferred*/)
-  {
-  }
+    void handle_write(const boost::system::error_code& /*error*/,
+        size_t /*bytes_transferred*/)
+    {
+    }
 
-  tcp::socket socket_;
-  std::string message_;
+    tcp::socket socket_;
+    std::string message_;
 };
