c17af8b73bde62595595e556fb15c72df0ca4df7
dragonpaulz <paul.geesaman@gmail.com>
2020-03-13 13:29:00 -0400

    Going from arrays to vector for handler

--- src/handler.cpp
+++ src/handler.cpp
@@ -2,8 +2,20 @@
 
 #include "handler.hpp"
 
+const std::vector<uint8_t> handler::Data::hello = std::vector<uint8_t>{uint8_t(240), uint8_t(16)}; // 0xE110
+const std::vector<uint8_t> handler::Data::data = {uint8_t(218), uint8_t(122)}; // 0xDA7A
+const std::vector<uint8_t> handler::Data::goodbye = {uint8_t(11), uint8_t(31)}; // 0x0B1E
+
+handler::Data::Data handler::Data::Create(std::vector<uint8_t> in)
+{
+    for (int i = 0; i < in.size(); i++)
+    {
+
+    }
+}
+
 // When a stream of bytes come in, this is the code that interprets the data, and returns a string of bytes
-std::string HandleData::DataHandler::ReadBytes(std::uint8_t[])
+std::string handler::Data::ReadBytes()
 {
     return "";
 }
--- src/handler.hpp
+++ src/handler.hpp
@@ -2,12 +2,26 @@
 #define handler_hpp_
 
 #include <string>
+#include <vector>
 
-namespace HandleData
+namespace handler
 {
-    class DataHandler{
-        // Takes bytes of an unspecified length, and returns a string to interpret data
-        std::string ReadBytes(std::uint8_t[]);
+    class Data{
+        public:
+        Data Create(std::vector<uint8_t> in);
+        // TODO: figure out a better way to represent the information, maybe with stoi?
+        const static std::vector<uint8_t> hello;
+        const static std::vector<uint8_t> data;
+        const static std::vector<uint8_t> goodbye;
+        
+        private:
+        uint8_t type[]; // 2 bytes
+        uint8_t length[]; // 4 bytes
+        uint8_t message[]; // variable number of bytes, based on length value
+        
+        bool valid;
+
+        std::string ReadBytes();
         // Write out bytes to a specified stream
         void WriteBytes(std::string);
     };
